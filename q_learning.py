d68e63001acec4ddcade50e1c025cc446d340c9c
emerso2000 <83967836+emerso2000@users.noreply.github.com>
2023-11-08 16:50:21 -0500

    Update q_learning.py

--- q_learning.py
+++ q_learning.py
@@ -8,23 +8,20 @@ from utils import *
 from car import *
 
 class Q_learning:
-    def __init__(self, game):
+    def __init__(self, game, car):
         self.game = game
-        self.car = Car(self)
-
+        self.car = car
         self.num_states = 4
         self.num_actions = 3
 
         self.q_table = np.zeros((self.num_states, self.num_actions))
     
     def update(self):
-        car_state = self.car.get_state()
-        x_pos, y_pos, velocity, angle = car_state
-        print(x_pos)
+        x, y, velocity, angle = self.car.get_state()
+        # print(x)
         self.termination_condition()
-
+        
     def termination_condition(self):
-        if self.car.check_collision_racetrack() == False:
+        if self.car.check_collision_racetrack() == True:
             print("terminate")
             self.car.reset_position()
-        
